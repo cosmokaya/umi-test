f942840d34c2a99dd16582c916816a69a04eb3e9
dgk <276877056@qq.com>
2022-03-14 17:44:43 +0800

    2345

--- .umirc.ts
+++ .umirc.ts
@@ -4,8 +4,9 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
+  base: '/umi-test/',
   publicPath: './',
-  routes: [{ path: '/umi-test', component: '@/pages/index' }],
+  routes: [{ path: '/', component: '@/pages/index' }],
   fastRefresh: {},
   hash: true,
 });
